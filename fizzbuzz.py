4507a12bd1629af9fcdde4c9b4685114e12039c8
Rafael Rodriguez <rafaandresrv@gmail.com>
2024-04-11 13:31:25 +0200

    agregue comentarios al programa fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,20 @@
+''' * Escribe un programa que muestre por consola (con un print) los
+* números de 1 a 100 (ambos incluidos y con un salto de línea entre
+* cada impresión), sustituyendo los siguientes:
+* - Múltiplos de 3 por la palabra "fizz".
+* - Múltiplos de 5 por la palabra "buzz".
+* - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".'''
+ 
 numero = 1
 while numero <= 100:
-     if numero % 3 == 0 and numero % 5 == 0:
+    if numero % 3 == 0 and numero % 5 == 0:
         print("fizzBuzz")
-     elif numero % 5 == 0:
+    elif numero % 5 == 0:
         print("buzz")
-     elif numero % 3 == 0:
+    elif numero % 3 == 0:
          print("fizz")
-     else:
+    else:
          print(numero)    
-     numero = numero + 1
+    numero = numero + 1
         
     
